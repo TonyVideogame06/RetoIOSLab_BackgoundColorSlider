27a9bd702a25ac0c77fe3c18e7ab78a1bad50337
Brian Aranda <0209486@up.edu.mx>
2020-08-27 15:33:58 -0500

    Initial Commit

---
+++ SliderColorStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderColorStoryboard
+//
+//  Created by Brian Aranda on 27/08/20.
+//  Copyright © 2020 Brian Aranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
