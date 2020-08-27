2e05754e1806442f4c55dececc90d59c02c61ca2
Brian Aranda <0209486@up.edu.mx>
2020-08-27 16:39:39 -0500

    Programa terminado. Uso de Sliders RGB con el Backgroud Color de IOS

--- SliderColorStoryboard/ViewController.swift
+++ SliderColorStoryboard/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueSliderColorRed: UILabel!
+    @IBOutlet weak var valueSliderColorGreen: UILabel!
+    @IBOutlet weak var valueSliderColorBlue: UILabel!
+    @IBOutlet weak var sliderColorRed: UISlider!
+    @IBOutlet weak var sliderColorBlue: UISlider!
+    @IBOutlet weak var sliderColorGreen: UISlider!
+    @IBAction func SliderAction(_ sender: AnyObject) {
+        self.view.backgroundColor = UIColor(red: CGFloat(sliderColorRed.value)/255,green: CGFloat(sliderColorGreen.value)/255, blue: CGFloat(sliderColorBlue.value)/255, alpha: 1)
+        valueSliderColorRed.text = String(sliderColorRed.value)
+        valueSliderColorGreen.text = String(sliderColorGreen.value)
+        valueSliderColorBlue.text = String(sliderColorBlue.value)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
